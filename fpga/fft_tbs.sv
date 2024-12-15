5afa90335f2243c6e5b2a9468284d0d63ec2c6b7
KP-1012 <66186073+KP-1012@users.noreply.github.com>
2024-12-15 17:30:26 -0500

    Update fft_tbs.sv

--- fpga/fft_tbs.sv
+++ fpga/fft_tbs.sv
@@ -1,46 +1,6 @@
 `default_nettype none
 
-// module agu_tb;
-
-//     localparam N_samples = 8;
-
-//     logic clk, rst_n, finish, start;
-//     logic [$clog2(N_samples)-1:0] addr1, addr2, addrT, stage;
-
-//     agu #(N_samples) TOP(.*);
-
-//     initial begin
-//         clk = 1'b0;
-//         forever #5 clk = ~clk;
-//     end
-
-//     initial begin
-//         #10000
-//         $display("@%0t: Error timeout! Finish never asserted.", $time);
-//         $finish;
-//     end
-
-//     initial begin
-//         rst_n = 1'b1;
-//         rst_n <= 1'b0;
-//         start <= 1'b0;
-//         @(posedge clk);
-//         @(posedge clk);
-//         rst_n <= #1 1'b1;
-//         @(posedge clk);
-//         start <= 1'b1;
-//         @(posedge clk);
-//         start <= 1'b0;
-
-//         $monitor("@%0t: Stage=%d, Level=%d, Addr1=%d, Addr2=%d, AddrT=%d", $time, TOP.stage, TOP.level, addr1, addr2, addrT);
-        
-//         @(posedge finish);
-
-//         $finish;
-//     end
-
-// endmodule 
-
+// Following is a simple testbench that checks the FFT operation
 module fft_tb;
 
     localparam N_samples = 8;
@@ -152,3 +112,45 @@ module fft_tb;
     end
 
 endmodule 
+
+// // Uncomment the following code if you need to test the AGU only 
+// module agu_tb;
+
+//     localparam N_samples = 8;
+
+//     logic clk, rst_n, finish, start;
+//     logic [$clog2(N_samples)-1:0] addr1, addr2, addrT, stage;
+
+//     agu #(N_samples) TOP(.*);
+
+//     initial begin
+//         clk = 1'b0;
+//         forever #5 clk = ~clk;
+//     end
+
+//     initial begin
+//         #10000
+//         $display("@%0t: Error timeout! Finish never asserted.", $time);
+//         $finish;
+//     end
+
+//     initial begin
+//         rst_n = 1'b1;
+//         rst_n <= 1'b0;
+//         start <= 1'b0;
+//         @(posedge clk);
+//         @(posedge clk);
+//         rst_n <= #1 1'b1;
+//         @(posedge clk);
+//         start <= 1'b1;
+//         @(posedge clk);
+//         start <= 1'b0;
+
+//         $monitor("@%0t: Stage=%d, Level=%d, Addr1=%d, Addr2=%d, AddrT=%d", $time, TOP.stage, TOP.level, addr1, addr2, addrT);
+        
+//         @(posedge finish);
+
+//         $finish;
+//     end
+
+// endmodule 
